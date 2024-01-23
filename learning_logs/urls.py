70f3a36afe2b03da978451763516a4edffbbe445
Sharad2001 <vsharad2001@gmail.com>
2024-01-23 17:55:12 +0530

    Ready for deployment to heroku.

---
+++ learning_logs/urls.py
@@ -0,0 +1,26 @@
+"""Defines URL patterns for learning logs."""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # Home page
+    path(r'', views.index, name='index'),
+
+    # Show all topics.
+    path(r'topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic.
+    path(r'topics/<int:topic_id>/', views.topic, name='topic'),
+
+    # Page for adding a new topic
+    path(r'new_topic/', views.new_topic, name='new_topic'),
+
+    # Page for adding new entry
+    path(r'new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    # Page for editing an entry
+    path(r'edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+]
---
+++ users/urls.py
@@ -0,0 +1,19 @@
+"""Defines URL patterns for users"""
+
+from django.urls import path
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    # Login page
+    path(r'login/', LoginView.as_view(), {'template_name': 'users/login.html'}, name='login'),
+
+    # Logout page
+    path(r'logout/', views.logout_view, name='logout'),
+
+    # Registration page
+    path(r'register/', views.register, name='register'),
+]
